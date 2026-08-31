19a1ca8c998bab0f9d86900e7bd0b09bc4f430db
andraadev <107798601+andraadev@users.noreply.github.com>
2026-08-31 20:18:18 +0700

    feat(destinations): add API route for destinations resource

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DestinationController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,3 +10,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('/users', UserController::class);
+Route::apiResource('/destinations', DestinationController::class);
