467d254ebabe64b98281287fadc0422cad1f1f1e
andraadev <107798601+andraadev@users.noreply.github.com>
2026-08-31 21:07:23 +0700

    feat(resources): add APIResource and clean up unused resources

--- app/Http/Resources/APIResource.php
+++ app/Http/Resources/APIResource.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class DestinationResource extends JsonResource
+class APIResource extends JsonResource
 {
-    //define properti
     public $status;
     public $message;
     public $resource;
