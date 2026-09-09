cce658fccd06bf69126f59b4758ac93f2ac89b6d
andraadev <107798601+andraadev@users.noreply.github.com>
2026-09-09 23:32:52 +0700

    fix(routes): update destinations API resource parameters for clarity

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('/users', UserController::class)->only('index');
-// Route::apiResource('/destinations', DestinationController::class)->parameters(['destinations' => 'slug'])->only(['index', 'show']);
 Route::apiResource('/destinations', DestinationController::class)->only(['index', 'show']);
 Route::apiResource('/booking', BookingController::class)->only(['index', 'store']);
 
