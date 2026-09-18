3e00500bef239b1146e5877287347dfad8702881
andraadev <107798601+andraadev@users.noreply.github.com>
2026-09-18 21:07:12 +0700

    feat(middleware): add CheckRole middleware

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (! $request->user() || ! in_array($request->user()->role, $roles)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Anda tidak memiliki hak akses untuk sumber daya ini.'
+            ], 403);
+        }
+
+        return $next($request);
+    }
+}
