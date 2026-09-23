9af668c94801bd08c9e6f8f46f86a372bdeda367
andraadev <107798601+andraadev@users.noreply.github.com>
2026-09-23 13:14:36 +0700

    refactor(app): streamline middleware configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,10 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->statefulApi();
-    })
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->statefulApi();
+
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
         ]);
